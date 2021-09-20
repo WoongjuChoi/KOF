800c4d1944d6ac3c019dd7036661370eaeb53b40
Woong Ju Choi <61194598+WoongjuChoi@users.noreply.github.com>
2021-09-20 20:37:23 +0900

    기반파일
    이딴거에 2시간이나 사용하다니...

---
+++ The_King_of_Fighters/The_King_of_Fighters/Config.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+#define WIN_START_POS_X	200
+#define WIN_START_POS_Y	50
+#define WIN_SIZE_X	1200
+#define WIN_SIZE_Y	700
+
+#define PI 3.14159265357989
+#define PI2 (3.14159265357989 * 2)
+
+#define DEGREE_TO_RADIAN(x)		(x * PI / 180.0f)
+
+#define SAFE_RELEASE(p)	{ if (p) { p->Release(); delete p; p = nullptr; } }
+
+enum MoveDir { Left, Right, Up, Down };
+
+extern HWND g_hWnd;
+extern HINSTANCE g_hInstance;
---
+++ The_King_of_Fighters/The_King_of_Fighters/MainGame.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "Config.h"
+#include "GameEntity.h"
+
+// 우리가 구성할 게임 월드
+class Image;
+class Iori;
+class MainGame : public GameEntity
+{
+private:
+	// 타이머
+	HANDLE hTimer;
+
+	// UI
+	char text[128];
+
+	int mousePosX;
+	int mousePosY;
+	int clickedMousePosX;
+	int clickedMousePosY;
+
+	// 백버퍼
+	Image* backBuffer;
+
+	// 배경 이미지
+	Image* backGround;
+
+	// 이오리
+	Iori* iori;
+
+public:
+	void Init();
+	void Update();
+	//void Render();			// 오버라이딩 : 상속 관계에서 부모클래스의 함수를 재정의
+	void Render(HDC hdc);	// 오버로딩
+	void Release();
+
+	LRESULT MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+};
